9de44b5ee4d9f1ab38a37df7275e27e367ae6f80
leofountas <leofoutas@gmail.com>
2024-11-21 12:29:55 +0100

    gameover Check works now

--- script.js
+++ script.js
@@ -2,12 +2,40 @@ const dino = document.getElementById('dino');
 
 const obstacle = document.getElementById('obstacle');
 
+const game = document.getElementById('game');
 
-const game = document.getElementById('game').addEventListener('click',function jump() { 
-    if(dino.classList != 'animate') {
-         dino.classList.add('animate');
-    };
-    setTimeout(function() {
-        dino.classList.remove('animate');
-    },500);
+// jump by mouse/finger click on game box
+game.addEventListener('click', function () {
+    jump();
 });
+
+// jump by clicking on Spacebar key
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space') {
+        jump();
+    }
+});
+
+// jump function add and remove the animate class to dino 
+function jump() {
+    if (dino.classList != 'animate') {
+        dino.classList.add('animate');
+    };
+    setTimeout(function () {
+        dino.classList.remove('animate');
+    }, 500);
+}
+
+const gameoverCheck = setInterval(function () {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+    if (obstacleLeft < 60 && obstacleLeft > 0 && dinoTop >= 280) {
+        obstacle.style.animation = 'none';
+        obstacle.style.display = 'none';
+        alert('you LOSE');
+        clearInterval(gameoverCheck);
+    }
+}, 10);
+
+
+
